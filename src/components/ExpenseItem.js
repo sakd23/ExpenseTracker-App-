465b73a3881a3ee6831bafad5d08536455b5c08a
sakd23 <sakshamdhasmana_ec20a15_81@dtu.ac.in>
2022-01-22 14:25:31 +0530

    learning useState hook practice

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
+import react, {useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
+
 function ExpenseItem(props){
 
-    const hoverHandler=()=>{
-        console.log("hovering");
-    }
+    const [title,setTitle]=useState(props.title);
+    // const hoverHandler=()=>{
+    //     console.log("hovering");
+    // }
 
    const clickHandler=()=>{
-       console.log("clicked")
+       console.log("clicked");
+       setTitle("Updated!");
    }
     return (
         <Card className="expense-item">
@@ -16,12 +20,12 @@ function ExpenseItem(props){
          <ExpenseDate date={props.date} />
 
         <div  className="expense-item__description">
-            <h2 >{props.title}</h2>
+            <h2 >{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         
         </div>
         
-        <button onMouseEnter={hoverHandler} onClick={clickHandler}>button</button>
+        <button  onClick={clickHandler}>button</button>
         </Card>
     )
 }
